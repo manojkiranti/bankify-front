0b6c0c7a18cde96922e5191a47b1d0b407dc2b9e
manoj-broker-copilot <manoj@odinmortgage.com>
2024-12-24 14:52:38 +0545

    updated slider

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -6,7 +6,9 @@ import { faAddressCard } from "@fortawesome/pro-light-svg-icons";
 import { Input, Typography, Collapse, Button } from 'antd';
 import type { CollapseProps } from 'antd';
 import { faSearch } from "@fortawesome/pro-light-svg-icons";
-import { SectionHeading } from "@/pages/home/routes/Home";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
 // import { Scrollbars } from "rc-scrollbars";
 
 // const { useBreakpoint } = Grid;
@@ -68,42 +70,77 @@ const Sidebar = () => {
 
               <div className="sidebar-cards" style={{padding:"0 1rem"}}>
                 <Typography.Title level={4} style={{marginTop:"55px"}}>Sample Services</Typography.Title>
-                <div className="cards-list">
-                  <div className="card-col">
-                      <div className="card-color bg-primary-1">
-                          <div className="card-color-body">
-                              <div className="card-icon-wrap">
-                                <div className="card-icon">
-                                    <FontAwesomeIcon icon={faAddressCard} />
-                                </div>
-                              </div>
-                              <div className="card-color-text">
-                                <Typography.Title level={5} style={{color:"#fff", marginTop:"0"}}>Lorem Ipsum</Typography.Title>
-                                <Typography.Title level={5} style={{color:"#fff", marginTop:"0.5rem", marginBottom:"1rem"}}> It is a long established fact that a reader will be distracted </Typography.Title>
-                                <Button>Lorem</Button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="card-col">
-                      <div className="card-color  bg-success-1">
-                          <div className="card-color-body">
-                              <div className="card-icon-wrap">
-                                <div className="card-icon">
-                                    <FontAwesomeIcon icon={faAddressCard} />
-                                </div>
-                              </div>
-                              <div className="card-color-text">
-                                <Typography.Title level={5} style={{color:"#fff", marginTop:"0"}}>Lorem Ipsum</Typography.Title>
-                                <Typography.Title level={5} style={{color:"#fff", marginTop:"0.5rem", marginBottom:"1rem"}}> It is a long established fact that a reader will be distracted </Typography.Title>
-                                <Button>Lorem</Button>
+
+                <div className="swiper-card-wrapper">
+                  <Swiper
+                  spaceBetween={10}
+                  slidesPerView="auto"
+                  onSlideChange={() => console.log('slide change')}
+                  onSwiper={(swiper) => console.log(swiper)}
+                >
+                    <SwiperSlide>
+
+                        <div className="card-col">
+                          <div className="card-color bg-primary-1">
+                              <div className="card-color-body">
+                                  <div className="card-icon-wrap">
+                                    <div className="card-icon">
+                                        <FontAwesomeIcon icon={faAddressCard} />
+                                    </div>
+                                  </div>
+                                  <div className="card-color-text">
+                                    <Typography.Title level={5} style={{color:"#fff", marginTop:"0"}}>Lorem Ipsum</Typography.Title>
+                                    <Typography.Title level={5} style={{color:"#fff", marginTop:"0.5rem", marginBottom:"1rem"}}> It is a long established fact that a reader will be distracted </Typography.Title>
+                                    <Button>Lorem</Button>
+                                  </div>
                               </div>
                           </div>
                       </div>
-                  </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="card-col">
+                        <div className="card-color  bg-success-1">
+                            <div className="card-color-body">
+                                <div className="card-icon-wrap">
+                                  <div className="card-icon">
+                                      <FontAwesomeIcon icon={faAddressCard} />
+                                  </div>
+                                </div>
+                                <div className="card-color-text">
+                                  <Typography.Title level={5} style={{color:"#fff", marginTop:"0"}}>Lorem Ipsum</Typography.Title>
+                                  <Typography.Title level={5} style={{color:"#fff", marginTop:"0.5rem", marginBottom:"1rem"}}> It is a long established fact that a reader will be distracted </Typography.Title>
+                                  <Button>Lorem</Button>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="card-col">
+                        <div className="card-color  bg-success-1">
+                            <div className="card-color-body">
+                                <div className="card-icon-wrap">
+                                  <div className="card-icon">
+                                      <FontAwesomeIcon icon={faAddressCard} />
+                                  </div>
+                                </div>
+                                <div className="card-color-text">
+                                  <Typography.Title level={5} style={{color:"#fff", marginTop:"0"}}>Lorem Ipsum</Typography.Title>
+                                  <Typography.Title level={5} style={{color:"#fff", marginTop:"0.5rem", marginBottom:"1rem"}}> It is a long established fact that a reader will be distracted </Typography.Title>
+                                  <Button>Lorem</Button>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                 </Swiper>
                 </div>
               </div>
+
+              
             </div>
+
+
       </div> 
     </>
   );
