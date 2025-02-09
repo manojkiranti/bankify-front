d1c106827dfd23c4e675b2f0d9a87c2c6592f0b7
manoj-broker-copilot <manoj@odinmortgage.com>
2025-02-09 16:24:30 +0545

    build file

---
+++ build/static/Container-BUC91SEG.js
@@ -0,0 +1,2 @@
+import{$ as r}from"./index-BwZTT435.js";const o={md:1200,sm:1024,lg:1320},c=({width:t,children:n,align:s="center",fullWidth:e=!1,style:a})=>{const i=t?o[t]:1320;return r.jsx("div",{style:{maxWidth:`${e?"100%":i}px`,margin:`${s==="center"?"0 auto":"0"}`,padding:"0 16px",width:"100%",...a},className:"main-container",children:n})};export{c as C};
+//# sourceMappingURL=Container-BUC91SEG.js.map
